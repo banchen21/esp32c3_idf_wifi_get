4bf186bbe0ffdeb6657ef759031adc5a3ef26e3f
banchen21 <banchen19@outlook.com>
2024-10-11 02:28:33 +0800

    add use crate esp32c3_wifi

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use core::str;
 
-use esp_idf_svc::{
-    eventloop::EspSystemEventLoop, 
-    hal::prelude::Peripherals, http::{client::{self, Configuration as HttpConfiguration, EspHttpConnection}, Method},
-     wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration as WifiConfiguration, EspWifi}};
-use log::info;
 use anyhow::{bail, Result};
+use esp32c3_wifi::wifi;
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::prelude::Peripherals,
+    http::{
+        client::{self, Configuration as HttpConfiguration, EspHttpConnection},
+        Method,
+    },
+    wifi::{
+        AuthMethod, BlockingWifi, ClientConfiguration, Configuration as WifiConfiguration, EspWifi,
+    },
+};
+use log::{debug, info};
 
 fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -14,73 +22,23 @@ fn main() -> Result<()> {
     let peripherals = Peripherals::take().unwrap();
     let sysloop = EspSystemEventLoop::take()?;
 
-    let ssid="Redmi Turbo 3";
-    let pass="";
-
+    let ssid = "test";
+    let pass = "";
     let auth_method = AuthMethod::None;
-    let mut esp_wifi = EspWifi::new(peripherals.modem, sysloop.clone(), None)?;
-    let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sysloop)?;
-    wifi.set_configuration(&WifiConfiguration::Client(ClientConfiguration::default()))?;
-
-    info!("Starting wifi...");
-
-    wifi.start()?;
-    info!("Scanning...");
-
-    let ap_infos = wifi.scan()?;
-
-    let ours = ap_infos.into_iter().find(|a| a.ssid == ssid);
-
-    let channel = if let Some(ours) = ours {
-        info!(
-            "Found configured access point {} on channel {}",
-            ssid, ours.channel
-        );
-        Some(ours.channel)
-    } else {
-        info!(
-            "Configured access point {} not found during scanning, will go with unknown channel",
-            ssid
-        );
-        None
-    };
-
-    wifi.set_configuration(&WifiConfiguration::Client(ClientConfiguration {
-        ssid: ssid
-            .try_into()
-            .expect("Could not parse the given SSID into WiFi config"),
-        password: pass
-            .try_into()
-            .expect("Could not parse the given password into WiFi config"),
-        channel,
-        auth_method,
-        ..Default::default()
-    }))?;
-
-    info!("连接wifi中");
-
-    wifi.connect()?;
-
-    info!("向wifi获取DHCP中");
-
-    wifi.wait_netif_up()?;
-
-    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
-
-    info!("Wifi DHCP 信息: {:?}", ip_info);
+    let _wifi = wifi(ssid, pass, auth_method, peripherals.modem, sysloop)?;
     get("http://neverssl.com/")?;
-
     loop {
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
+
 fn get(url: impl AsRef<str>) -> Result<()> {
     let config = HttpConfiguration::default();
     let connection = EspHttpConnection::new(&config)?;
     let mut client = embedded_svc::http::client::Client::wrap(connection);
     let headers = [("accept", "text/plain")];
 
-    let request=client.request(Method::Get, url.as_ref(), &headers)?;
+    let request = client.request(Method::Get, url.as_ref(), &headers)?;
     let response = request.submit()?;
     let status = response.status();
     println!("Response code: {}\n", status);
